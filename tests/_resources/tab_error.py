2afe398629d0505dfa1b5ad7d13eb68a3df695bf
Alex Hughes <ahughesalex@gmail.com>
2015-10-17 00:10:43 -0700

    Adding some commenting to the tab_error file. Also testing closing an issue
    Closes #3

--- tests/_resources/tab_error.py
+++ tests/_resources/tab_error.py
@@ -2,12 +2,12 @@ __author__ = 'Alex'
 
 
 def boop( my_value ):  # Has spaces around both.
-	print my_value
+	print my_value  # Has a tab
 
 
 def beep():
-	value = "Hello"
-	boop(value )  # Space only on the end.
+	value = "Hello"  # Has a tab
+	boop(value )  # Space only on the end. Has a tab
 
 if __name__ == "__main__":
-	beep( )  # Spaces between empty braces.
+	beep( )  # Spaces between empty braces. Has a tab
